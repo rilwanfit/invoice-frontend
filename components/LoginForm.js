69e8fe9f550465d66b8282b81a71f3e10e45f68e
MH Rilwan <rilwanfit@gmail.com>
2020-03-27 22:48:22 +0100

    Add register support

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import fetch from 'isomorphic-unfetch'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { login } from '../utils/auth'
@@ -7,112 +6,110 @@ import { login } from '../utils/auth'
 const apiUrl = 'https://localhost:8000/login_check'
 
 const RegisterSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Invalid email address format")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(3, "Password must be 3 characters at minimum")
-    .required("Password is required")
+    email: Yup.string()
+        .email("Invalid email address format")
+        .required("Email is required"),
+    password: Yup.string()
+        .min(3, "Password must be 3 characters at minimum")
+        .required("Password is required")
 });
 
 const Login = ({ username, password }) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log(username, password);
-        try {
-            const response = fetch(apiUrl, {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ username, password }, null, 2)
-            })
-            if (response.ok) {
-              const { token } = response.json()
-              login({ token })
-            } else {
-              reject(new Error('Login failed.'));
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                fetch(apiUrl, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ username, password }, null, 2)
+                })
+                .then((response) => {
+                    if (response.ok) {
+                        const { token } = response.json();
+                        login({ token })
+                    } else {
+                        reject(new Error('Login failed.'));
+                    }
+                })
+            } catch (error) {
+                reject(new Error('You have an error in your code or there are Network issues.'));
             }
-          } catch (error) {
-            reject(new Error('You have an error in your code or there are Network issues.'));
-          }
-      resolve(true);
-    }, 1000);
-  });
+            resolve(true);
+        }, 1000);
+    });
 
 const RegisterForm = () => {
-  return (
-    <div className="container">
-      <div className="row mb-5">
-        <div className="col-lg-12 text-center">
-          <h1 className="mt-5">Login Form</h1>
+    return (
+        <div className="container">
+            <div className="row mb-5">
+                <div className="col-lg-12 text-center">
+                    <h1 className="mt-5">Login Form</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <Formik
+                        initialValues={{ email: "", password: "" }}
+                        validationSchema={RegisterSchema}
+                        onSubmit={(values, { setSubmitting, setFieldError }) => {
+                            Login({ username: values.email, password: values.password })
+                                .catch(error => {
+                                    setFieldError('general', error.message);
+                                })
+                                .finally(() => {
+                                    setSubmitting(false);
+                                });
+                        }}
+                    >
+                        {({ touched, errors, isSubmitting }) => (
+                            <Form>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email</label>
+                                    <Field
+                                        type="email"
+                                        name="email"
+                                        placeholder="Enter email"
+                                        className={`form-control ${
+                                            touched.email && errors.email ? "is-invalid" : ""
+                                            }`}
+                                    />
+                                    <ErrorMessage
+                                        component="div"
+                                        name="email"
+                                        className="invalid-feedback"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <Field
+                                        type="password"
+                                        name="password"
+                                        placeholder="Enter password"
+                                        className={`form-control ${
+                                            touched.password && errors.password ? "is-invalid" : ""
+                                            }`}
+                                    />
+                                    <ErrorMessage
+                                        component="div"
+                                        name="password"
+                                        className="invalid-feedback"
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary btn-block"
+                                    disabled={isSubmitting}
+                                >
+                                    {isSubmitting ? "Please wait..." : "Login"}
+                                </button>
+                                <div style={{ color: 'red' }}>{errors.general}</div>
+                            </Form>
+                        )}
+                    </Formik>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={RegisterSchema}
-            onSubmit={(values, { setSubmitting, setFieldError }) => {
-                Login({ username: values.email, password: values.password })
-                    .then(() => {
-                        
-                    })
-                    .catch(error => {
-                        setFieldError('general', error.message);
-                    })
-                    .finally(() => {
-                        setSubmitting(false);
-                    });
-            }}
-          >
-          {({ touched, errors, isSubmitting }) => (
-            <Form>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Field
-                  type="email"
-                  name="email"
-                  placeholder="Enter email"
-                  className={`form-control ${
-                    touched.email && errors.email ? "is-invalid" : ""
-                  }`}
-                />
-                <ErrorMessage
-                  component="div"
-                  name="email"
-                  className="invalid-feedback"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Field
-                  type="password"
-                  name="password"
-                  placeholder="Enter password"
-                  className={`form-control ${
-                    touched.password && errors.password ? "is-invalid" : ""
-                  }`}
-                />
-                <ErrorMessage
-                  component="div"
-                  name="password"
-                  className="invalid-feedback"
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-primary btn-block"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Please wait..." : "Login"}
-              </button>
-              <div style={{ color: 'red' }}>{errors.general}</div>
-            </Form>
-          )}           
-          </Formik>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RegisterForm;
--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+const apiUrl = 'https://localhost:8000/register'
+
 const RegisterSchema = Yup.object().shape({
   email: Yup.string()
     .email("Invalid email address format")
@@ -10,6 +12,31 @@ const RegisterSchema = Yup.object().shape({
     .min(3, "Password must be 3 characters at minimum")
     .required("Password is required")
 });
+
+const Register = ({ _username, _password }) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        fetch(apiUrl, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ _username, _password }, null, 2),
+          mode: 'cors'
+        })
+          .then((response) => {
+            if (response.ok) {
+              // const { token } = response.json()
+              // login({ token })
+            } else {
+              reject(new Error('Registration failed.'));
+            }
+          })
+      } catch (error) {
+        reject(new Error('You have an error in your code or there are Network issues.'));
+      }
+      resolve(true);
+    }, 1000);
+  });
 
 const RegisterForm = () => {
   return (
@@ -24,54 +51,64 @@ const RegisterForm = () => {
           <Formik
             initialValues={{ email: "", password: "" }}
             validationSchema={RegisterSchema}
-            onSubmit={({ setSubmitting }) => {
-              alert("Form is validated! Submitting the form...");
-              setSubmitting(false);
+            onSubmit={(values, { setSubmitting, setFieldError }) => {
+              Register({ _username: values.email, _password: values.password })
+                .then((response) => {
+                  // const { token } = response;
+                  // login({ token })
+                })
+                .catch(error => {
+                  setFieldError('general', error.message);
+                })
+                .finally(() => {
+                  setSubmitting(false);
+                });
             }}
           >
-          {({ touched, errors, isSubmitting }) => (
-            <Form>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Field
-                  type="email"
-                  name="email"
-                  placeholder="Enter email"
-                  className={`form-control ${
-                    touched.email && errors.email ? "is-invalid" : ""
-                  }`}
-                />
-                <ErrorMessage
-                  component="div"
-                  name="email"
-                  className="invalid-feedback"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Field
-                  type="password"
-                  name="password"
-                  placeholder="Enter password"
-                  className={`form-control ${
-                    touched.password && errors.password ? "is-invalid" : ""
-                  }`}
-                />
-                <ErrorMessage
-                  component="div"
-                  name="password"
-                  className="invalid-feedback"
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-primary btn-block"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Please wait..." : "Submit"}
-              </button>
-            </Form>
-          )}           
+            {({ touched, errors, isSubmitting }) => (
+              <Form>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    type="email"
+                    name="email"
+                    placeholder="Enter email"
+                    className={`form-control ${
+                      touched.email && errors.email ? "is-invalid" : ""
+                      }`}
+                  />
+                  <ErrorMessage
+                    component="div"
+                    name="email"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Field
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    className={`form-control ${
+                      touched.password && errors.password ? "is-invalid" : ""
+                      }`}
+                  />
+                  <ErrorMessage
+                    component="div"
+                    name="password"
+                    className="invalid-feedback"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? "Please wait..." : "Submit"}
+                </button>
+                <div style={{ color: 'red' }}>{errors.general}</div>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
