4da8a371e1ed264991da747f03ab26175fc681c5
Menapal89 <a.menapal@live.nl>
2020-03-28 14:32:52 +0100

    add-new-page

---
+++ pages/invoice.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function Invoice() {
+    return (
+        <Layout>
+            <p>This is the Invoice page</p>
+        </Layout>
+    );
+  }
