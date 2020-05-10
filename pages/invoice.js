5175c26eebbf34f4a5d93bff3b8ca155260032a2
MH Rilwan <rilwanfit@gmail.com>
2020-05-11 01:32:43 +0200

    top bar and invoice table

--- pages/invoice.js
+++ pages/invoice.js
@@ -1,10 +1,11 @@
 import Layout from '../components/Layout';
 import InvoiceForm from '../components/InvoiceForm';
+import InvoiceList from '../components/InvoiceList';
 
 export default function Invoice() {
     return (
         <Layout>
-            <InvoiceForm/>
+            <InvoiceList />
         </Layout>
     );
   }
